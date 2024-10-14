7f8c988eb2070f21892ba4c7be684242ea87475c
Jorge <jorge.casanova.sanchez@gmail.com>
2024-10-14 12:48:10 +0200

    Boton de acción "LogOut"

--- src/main/java/com/example/gearfit/controllers/MainViewController.java
+++ src/main/java/com/example/gearfit/controllers/MainViewController.java
@@ -1,7 +1,18 @@
 package com.example.gearfit.controllers;
 
+import javafx.animation.ScaleTransition;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.util.Duration;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -10,6 +21,23 @@ public class MainViewController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+
     }
 
+    public void logOutSession(ActionEvent event) {
+        try {
+            Parent mainView = FXMLLoader.load(getClass().getResource("/com/example/gearfit/UserAuth.fxml"));
+
+            // Crear la escena y establecer el fondo como transparente
+            Scene mainScene = new Scene(mainView);
+            mainScene.setFill(Color.TRANSPARENT); // Establecer fondo transparente
+
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+            stage.setScene(mainScene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
